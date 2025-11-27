d10a777669770972b8a4f03f08be8d5827d139f4
Arafat <mdarafat3167@gmail.com>
2025-11-27 23:34:50 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+      <h1>hello world</h1>
+    </div>
+  );
+}
